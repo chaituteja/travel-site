2b16c414fe3d4e708abc70aac540bb094ccb02c8
Teja <j.chaitanyateja@gmail.com>
2017-07-10 19:52:40 +1000

    Updated Gulp file and installed postCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,7 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function () {
-   console.log('Gulp task is started');
-});
 
-gulp.task('html', function () {
-   console.log("Trying new gulp task");
-});
 
-gulp.task('styles', function () {
-   console.log("SASS or PostCSS tasks");
-});
 
-gulp.task('watch', function () {
 
-   watch('./app/index.html', function () {
-      gulp.start('html');
-   });
-
-   watch('./app/assets/styles/**/*.css', function () {
-      gulp.start('styles');
-   });
-});
